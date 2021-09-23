7890b8e7d60014212fdb5c9a1b8a0668f7881d12
Yohannes Getu <nede.evoli@gmail.com>
2021-09-22 22:05:21 -0700

    changed STDERR_FILENO to STDOUT_FILENO

--- push.c
+++ push.c
@@ -12,8 +12,12 @@ static int check_for_digit(char *arg)
 	int i;
 
 	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
 		if (isdigit(arg[i]) == 0)
 			return (1);
+	}
 	return (0);
 }
 
@@ -32,7 +36,7 @@ void m_push(stack_t **stack, unsigned int line_number)
 	arg = strtok(NULL, "\n\t\r ");
 	if (arg == NULL || check_for_digit(arg))
 	{
-		dprintf(STDERR_FILENO,
+		dprintf(STDOUT_FILENO,
 			"L%u: usage: push integer\n",
 			line_number);
 		exit(EXIT_FAILURE);
